e7d404356dbe90e4d0910f6a9d90f7cba91e58be
Asim <asimmahmood11110@gmail.com>
2025-12-03 16:53:11 +0500

    feat: Implement API client with CSRF handling and caching, and configure API rewrites for API and CSRF cookie endpoints.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://erp-server-main-xegmvt.laravel.cloud/api/:path*',
+      },
+      {
+        source: '/sanctum/csrf-cookie',
+        destination: 'https://erp-server-main-xegmvt.laravel.cloud/sanctum/csrf-cookie',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
